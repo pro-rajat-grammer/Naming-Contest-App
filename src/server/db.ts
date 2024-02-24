58866d9a132d418f9729f15084243a582e4f1b6c
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:29:34 +0530

    final commit

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,23 +1,16 @@
-
 import { MongoClient } from "mongodb";
-
 import { MONGODB_URL, DATABASE_NAME } from "./config";
 
-
 let connectedClient;
-
 export const connectClient = async () => {
     if (connectedClient) {
         return connectedClient.db(DATABASE_NAME)
-
     }
     const client = new MongoClient(MONGODB_URL);
     await client.connect();
     await client.db(DATABASE_NAME).command({ ping: 1 })
     console.info("connected to mongodb");
-
     connectedClient = client;
-
     return connectedClient.db(DATABASE_NAME)
 };
 
