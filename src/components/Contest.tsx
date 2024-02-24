db4f49c389b12e9bf782a7807ce806587c237aa3
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:32:19 +0530

    final commit

--- src/components/Contest.tsx
+++ src/components/Contest.tsx
@@ -5,15 +5,11 @@ import Header from './Header'
 const Contest = ({ initialContest, onContestListClick }) => {
     const [contest, setContest] = useState(initialContest);
     // console.log(contest);
-
     useEffect(() => {
         if (!contest.names) {
             fetchContest(contest.id).then((contest) => {
                 setContest(contest)
-            })
-        }
-        // console.log(setContest);
-        
+            })     } 
     }, [contest.id, contest.names]);
 
     const handleClickContestClick = (event) => {
@@ -23,26 +19,19 @@ const Contest = ({ initialContest, onContestListClick }) => {
 
     const handleNewNameSubmit = async (event) => {
         event.preventDefault();
-        const newNameInput = event.target.newName;
-        // console.log(newNameInput.value);
-        
+        const newNameInput = event.target.newName;  
         const updatedContest = await addNewNameToContest({
             contestId: contest.id,
             newNameValue: newNameInput.value})
         // console.log(updatedContest);
         setContest(updatedContest)
-
     };
-
-
+    
     return (
-
         <>  <Header message={contest.contestName} />
             <div className='contest'>
-
                 <div className="title">Contest Description</div>
                 <div className="description">{contest.description}</div>
-
                 <div className="title">Proposed names</div>
                 <div className="body">
                     {contest.names?.length > 0 ?
@@ -54,7 +43,6 @@ const Contest = ({ initialContest, onContestListClick }) => {
                         :
                         (<div>No names proposed yet</div>)
                     }</div>
-
                 <div className="title">Proposed a New Name</div>
                 <div className="body">
                     <form onSubmit={handleNewNameSubmit} >
@@ -62,12 +50,8 @@ const Contest = ({ initialContest, onContestListClick }) => {
                         <button type="submit">Submit</button>
                     </form>
                 </div>
-
-
                 <a href="/" className='link' onClick={handleClickContestClick} > Contest List</a>
-
             </div></>
     )
 }
-
 export default Contest
