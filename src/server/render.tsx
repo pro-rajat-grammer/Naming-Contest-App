5f66d8adfba40f255c1e48402c7fbb66d0051cb1
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:29:59 +0530

    Update render.tsx

--- src/server/render.tsx
+++ src/server/render.tsx
@@ -2,19 +2,14 @@ import { fetchContest, fetchContestList } from "../api-client"
 import ReactDOMServer from "react-dom/server"
 import App from "../components/App";
 
-
 const serverRender = async (req) => {
-
     const { contestId } = req.params;
-
     const initialData = contestId
         ? { currentContest: await fetchContest(contestId) }
         : { contests: await fetchContestList()}
 
-
     const initialMarkup = ReactDOMServer.renderToString(
         <App initialData={initialData } />);
-
     return { initialMarkup,initialData};
 }
 
